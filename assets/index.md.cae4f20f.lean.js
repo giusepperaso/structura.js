1c7bc2919114d9ca5ae3730da4eeb58fa00fa526
GiuseppeRaso <GiuseppeRaso@users.noreply.github.com>
2022-10-15 03:12:33 +0000

    deploy: e97cf0b80797d8811bcf8611004b471c3de3790b

--- assets/index.md.cae4f20f.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as r,a}from"./app.4358f8ff.js";const f=JSON.parse('{"title":"Structura.js","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),s={name:"index.md"},i=a("",6),o=[i];function l(n,c,h,u,p,m){return r(),t("div",null,o)}const b=e(s,[["render",l]]);export{f as __pageData,b as default};
