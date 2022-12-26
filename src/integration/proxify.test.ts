65935a076d7af81d35049a34721ad53360430707
GiuseppeRaso <54646766+GiuseppeRaso@users.noreply.github.com>
2022-12-26 15:35:18 +0100

    solve type problem in proxify test

--- src/integration/proxify.test.ts
+++ src/integration/proxify.test.ts
@@ -1,10 +1,10 @@
 import { vi, describe, expect, it } from "vitest";
-import { produce, createProxy } from "..";
+import { produce, createProxy, CreateProxy } from "..";
 
 describe.concurrent("proxify tests", async () => {
   it("should not proxify methods because it's useless", async () => {
     const _ = { createProxy };
-    const proxify = vi.spyOn(_, "createProxy");
+    const proxify = vi.spyOn(_, "createProxy") as unknown as CreateProxy;
     const myObj = [0, 1];
     const result = produce(
       myObj,
