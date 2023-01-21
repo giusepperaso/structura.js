3c73300e4460f46178ffdb0efce5cb8372aee987
GiuseppeRaso <54646766+GiuseppeRaso@users.noreply.github.com>
2023-01-21 01:34:48 +0100

    test symbols in strict copy

--- src/integration/settings.test.ts
+++ src/integration/settings.test.ts
@@ -1,17 +1,21 @@
 import { describe, expect, it } from "vitest";
-import { enableStrictCopy, produce } from "..";
+import { enableStrictCopy /* , produce */ } from "..";
+import { produceTest as produce } from "./utils";
 
 describe.concurrent(
   "verify that everything is working also with settings enabled",
   () => {
     it("also works with enabledStrictCopy", async () => {
       enableStrictCopy(true);
-      const myObj = { test1: [1] };
+      const symbol = Symbol();
+      const myObj = { test1: [1], [symbol]: "test" };
       const result = produce(myObj, (draft) => {
         draft.test1.push(1);
+        draft[symbol] = "test2";
       });
       expect(result).not.toBe(myObj);
       expect(result.test1).not.toBe(myObj.test1);
+      expect(result[symbol]).not.toBe(myObj[symbol]);
       enableStrictCopy(false);
     });
   }
