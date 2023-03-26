23c0691b816dd45f731fd0bde6ead097eb5d1fd1
GiuseppeRaso <54646766+GiuseppeRaso@users.noreply.github.com>
2023-03-26 13:19:53 +0200

    correct typing of test to reflect simplification of createProxy

--- src/integration/proxify.test.ts
+++ src/integration/proxify.test.ts
@@ -1,11 +1,11 @@
 import { vi, expect, it } from "vitest";
-import { produce, createProxy, CreateProxy } from "..";
+import { produce, createProxy } from "..";
 import { runMultiple } from "./utils";
 
 runMultiple("proxy and traps tests", async () => {
   it("should not proxify methods because it's useless", async () => {
     const _ = { createProxy };
-    const proxify = vi.spyOn(_, "createProxy") as unknown as CreateProxy;
+    const proxify = vi.spyOn(_, "createProxy") as unknown as typeof createProxy;
     const myObj = [0, 1];
     const result = produce(
       myObj,
